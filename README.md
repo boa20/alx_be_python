20f953e7a14a9dc3b8f3c95912d2f2cf722d9c4e
boa20 <kwesikyeremateng5@gmail.com>
2024-06-21 15:26:50 -0700

    Created repository and added directory for python introduction modlue

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository is to be used to learn Python programming
+
+- Module 1 is about the introduction to python. The module introduces basic concepts of Python3 programming language like datatypes, variables and operations.
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10  #Assign the value "10" to the variable 'number1'
+number2 = 5   #Assign the value "5" to the variable 'number2'
+
+sum = number1 + number2 #Define the sum of  'number1' and 'number2'
+difference = number1 - number2 #Define the difference between 'number1' and 'number2'
+product = number1 * number2 #Define the product of 'number1' and 'number2'
+
+print(f"The sum of {number1} and {number2} is {sum}")
+print(f"The difference between {number1} and {number2} is {difference}")
+print(f"The product of {number1} and {number2} is {product}")
