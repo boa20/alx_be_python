15e8a076caa1ad36b6b2dbc4383b2e2d060c91f2
boa20 <kwesikyeremateng5@gmail.com>
2024-06-21 15:45:07 -0700

    Python code to calculate simple interest was added.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000 #Principal amount of value '$1000'
+rate = 0.05 #Interest rate of value '5%'
+time = 3 #Period for the investment for '3years'
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
