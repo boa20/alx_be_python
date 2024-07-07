debdc2f6f9cc0e59f546f86ea3d3a2478bb397ee
boa20 <kwesikyeremateng5@gmail.com>
2024-07-07 20:15:42 +0000

    Added Shopping List Manager and Explore Datetime code

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(f"Current date and time: {current_date.strftime('%Y-%m-%d %X')}")
+
+display_current_datetime()
+
+number_of_days = int(input("Enter the number of days to add to the current date: "))
+
+def calculate_future_date(number_of_days):
+    future_date = datetime.datetime.now() + datetime.timedelta(days=number_of_days)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+
+calculate_future_date(number_of_days)
