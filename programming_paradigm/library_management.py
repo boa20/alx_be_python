af1c57a7d9e2c5e63afb18024d68a2af8fa58bf1
boa20 <kwesikyeremateng5@gmail.com>
2024-07-20 19:27:23 +0000

    Added Library Management code

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = True
+
+    def get_title(self):
+        return self.title
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+        self.book = Book(title="any", author="any")
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        self.title = title
+        for book in self._books:
+            if book.title == title:
+                #book._is_checked_out()
+                self.book = book
+                self._books.remove(book)
+        
+
+    def return_book(self, title):
+        self.title = title
+        if self.book.title == title:
+            self._books.append(self.book)
+
+    def list_available_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
+        
+    
