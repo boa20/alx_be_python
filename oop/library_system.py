56aeeb2b844e22f15e9b519d6ec21e1376214a17
boa20 <kwesikyeremateng5@gmail.com>
2024-07-21 19:38:54 +0000

    Added Library System code and a main-2 code for testing library system

---
+++ oop/library_system.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self, books=[]):
+        self.books = books
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            cls = book.__class__.__name__
+            match cls:
+                case "Book":
+                    print(f"{cls}: {book.title} by {book.author}")
+                
+                case "EBook":
+                    print(f"{cls}: {book.title} by {book.author}, File Size: {book.file_size}KB")
+                
+                case "PrintBook":
+                    print(f"{cls}: {book.title} by {book.author}, Page Count: {book.page_count}")
